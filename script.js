a9a7c51bcc477693129cb5678794dd5aec8a8233
TatianaNikulina <smirnovatatianav@gmail.com>
2022-03-21 14:21:53 +0100

    added like-dislike

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //open edit popup
 
-let editButton = document.querySelector(".button_type_edit");
-let editPopup = document.querySelector(".popup_type_edit");
+const editButton = document.querySelector(".button_type_edit");
+const editPopup = document.querySelector(".popup_type_edit");
 
 function openEditPopup() {
   editPopup.classList.add("popup_opened");
@@ -9,8 +9,8 @@ function openEditPopup() {
 editButton.addEventListener("click", openEditPopup);
 
 // open add popup
-let addButton = document.querySelector(".button_type_add");
-let addPopup = document.querySelector(".popup_type_add");
+const addButton = document.querySelector(".button_type_add");
+const addPopup = document.querySelector(".popup_type_add");
 
 function openAddPopup() {
   addPopup.classList.add("popup_opened");
@@ -18,7 +18,7 @@ function openAddPopup() {
 addButton.addEventListener("click", openAddPopup);
 
 // close popups
-let closeButton = document.querySelectorAll(".button_type_close");
+const closeButton = document.querySelectorAll(".button_type_close");
 
 function closePopup() {
   document.querySelectorAll(".popup").forEach((popup) => {
@@ -27,4 +27,14 @@ function closePopup() {
 }
 closeButton.forEach((btn) => {
   btn.addEventListener("click", closePopup);
+});
+
+// like - dislike
+
+const likeButton = document.querySelectorAll(".button_type_like");
+likeButton.forEach((btn) => {
+  btn.addEventListener("click", function (evt) {
+    evt.target.classList.toggle("button_active");
+    console.log(evt.target);
+  });
 });
